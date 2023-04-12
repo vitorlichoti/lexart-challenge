59a8941092d0cb30c400ee6b59f67e6729b4ac36
Vitor Lichoti <vitorlichoti@gmail.com>
2023-04-12 16:01:53 -0400

    Fix: bug na requisição

--- front-end/src/utils/fetch.js
+++ front-end/src/utils/fetch.js
@@ -3,5 +3,11 @@ import axios from 'axios';
 const HOST = process.env.REACT_APP_API_HOST || "localhost";
 const PROTOCOL = process.env.REACT_APP_API_PROTOCOL || "http";
 
-const productsMeliApi = async (method, endpoint) => axios[method](`${PROTOCOL}://${HOST}`);
+const productsMeliApi = async (method, endpoint) => {
+  try {
+    return await axios[method](`${PROTOCOL}://${HOST}/${endpoint}`);
+  } catch (error) {
+    return error.message;
+  }
+}
 export default productsMeliApi
